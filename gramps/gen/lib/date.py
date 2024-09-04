df6b2476cf8facd5b11d2aaac0855a2b4ca1a0cc
CameronD73 <56762299+CameronD73@users.noreply.github.com>
2024-09-04 23:33:44 +1000

    minor documentation addition - clarify that if the date has two parts then copy_offset_ymd() will apply the offset to both parts

--- gramps/gen/lib/date.py
+++ gramps/gen/lib/date.py
@@ -1486,6 +1486,7 @@ class Date:
     def set_yr_mon_day_offset(self, year=0, month=0, day=0):
         """
         Offset the date by the given year, month, and day values.
+        If the source is a compound date then both are offset.
         """
         if self.__set_yr_mon_day_offset(
             year, month, day, Date._POS_YR, Date._POS_MON, Date._POS_DAY
@@ -1509,6 +1510,7 @@ class Date:
     def copy_offset_ymd(self, year=0, month=0, day=0):
         """
         Return a Date copy based on year, month, and day offset.
+        If the source is a compound date then both are offset.
         """
         orig_cal = self.calendar
         if self.calendar != 0:
